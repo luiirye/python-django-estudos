4c286bdcfc573f56beb33e0cddefd75a414a97ba
Luiirye <borg.esluii01645@gmail.com>
2026-02-24 17:23:33 -0400

    App, Model criado e Banco de Dados configurado. Projeto: estante_livros

--- minha_estante/livros/models.py
+++ minha_estante/livros/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Livro(models.Model):
+    STATUS_CHOICES = [
+        ('quero_ler', 'Quero Ler'),
+        ('lendo', 'Lendo'),
+        ('lido', 'Lido'),
+    ]
+
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='quero_ler')
+    data_adicao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self): ## Método especial que define como o objeto (Livro) vai ser tratado.
+        return self.titulo
+
+
+
+
--- minha_estante/minha_estante/settings.py
+++ minha_estante/minha_estante/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'livros', ##Adicionado o App Livros.
 ]
 
 MIDDLEWARE = [
