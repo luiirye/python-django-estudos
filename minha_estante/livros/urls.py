c61028fbfe636f7a9f23c73b8859435590534df8
Luiirye <borg.esluii01645@gmail.com>
2026-02-25 17:15:54 -0400

    Funções de editar, adicionar, deletar livros - Finalizado projeto minha estante de livros com Djanho

--- minha_estante/livros/urls.py
+++ minha_estante/livros/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.lista_livros, name='lista_livros'),
+    path('<int:pk>/', views.detalhe_livro, name='detalhe_livro'),
+    path('adicionar/', views.adicionar_livro, name='adicionar_livro'),
+    path('<int:pk>/editar/', views.editar_livro, name='editar_livro'),
+    path('<int:pk>/deletar/', views.deletar_livro, name='deletar_livro'),
 ]
--- minha_estante/livros/views.py
+++ minha_estante/livros/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Livro
+from .forms import LivroForm
 
-# Create your views here.
+def lista_livros(request):
+    livros = Livro.objects.all()
+    return render(request, 'livros/lista.html', {'livros': livros})
+def detalhe_livro(request, pk):
+    livro = get_object_or_404(Livro, pk=pk)
+    return render(request, 'livros/detalhe.html', {'livro': livro})
+
+def adicionar_livro(request):
+    if request.method == 'POST':
+        form = LivroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_livros')
+    else:
+        form = LivroForm()
+    return render(request, 'livros/adicionar.html', {'form': form})
+
+def editar_livro(request, pk):
+    livro = get_object_or_404(Livro, pk=pk)
+    if request.method == 'POST':
+        form = LivroForm(request.POST, instance=livro)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_livros')
+    else:
+        form = LivroForm(instance=livro)
+    return render(request, 'livros/editar.html', {'form': form})
+
+def deletar_livro(request, pk):
+    livro = get_object_or_404(Livro, pk=pk)
+    if request.method == 'POST':
+        livro.delete()
+        return redirect('lista_livros')
+    return render(request, 'livros/deletar.html', {'livro': livro})
